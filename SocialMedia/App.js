4759d7b0f3572e0a61e8db62aa3fe06f242b353f
FahriNazarudin <fahrinazarudin0405@gmail.com>
2025-06-11 16:55:37 +0700

    Add initial package.json for SocialMedia project with dependencies and scripts

---
+++ SocialMedia/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>HAHA HIHI HUHU</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "black",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
